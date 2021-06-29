5974059a758a795b7f7a1a2444c1e744aa949394
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-06-29 18:10:56 -0300

    Create spring project

---
+++ backend/src/main/java/com/mapin/tropical/TropicalApplication.java
@@ -0,0 +1,13 @@
+package com.mapin.tropical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TropicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TropicalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mapin/tropical/TropicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapin.tropical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TropicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
